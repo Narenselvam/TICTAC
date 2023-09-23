5f879f9482a5675bdb17b2511f22e7895724889e
Narenselvam <narenkishore75@gmail.com>
2023-09-23 10:39:18 +0530

    clean logic

--- glue.js
+++ glue.js
@@ -1,46 +1,63 @@
-
-let btnRef = document.querySelectorAll(".playBox");
-
-let tempVal=""
-let tempId=0;
+// Initial game setup
+let Box = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+];
 let playerX = true;
 
-function indexing(tempId,tempVal){
-    let x=parseInt(tempId[0]);
-    let y=parseInt(tempId[1]);
-    Box[x][y]=tempVal;
-    console.log(Box)
-}
-
-let Box = [
-    ["", "", ""],//(0,0),(0,1)
-    ["", "", ""],//(1,0),(1,1)
-    ["", "", ""],//(2,0),(2,1)
+// Winning combinations
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ];
-console.log(Box);
 
+// Event listener for the buttons
+let btnRef = document.querySelectorAll(".playBox");
 btnRef.forEach((element)=>{
     element.addEventListener("click",()=>{
-
-        if(playerX){
-            playerX=false;
-            element.innerText="X";
-            element.disabled = true;
-            let tempVal=element.innerText;
-            let tempId=element.getAttribute("id");
-            console.log(tempId);
-            indexing(tempId,tempVal);
-
-        }
-
-        else{
-            playerX=true;
-            element.innerText="O";
-            element.disabled = true;
-            let tempVal=element.innerText;
-            let tempId=element.getAttribute("id");
-            console.log(tempId);
-            indexing(tempId,tempVal);
-        }
+        let tempVal = playerX ? "X" : "O";
+        let tempId = element.getAttribute("id");
+        indexing(tempId, tempVal);
+        element.innerText = tempVal;
+        element.disabled = true;
+        playerX = !playerX;
     })
 })
+
+// Update game state
+function indexing(tempId, tempVal){
+    let coordinates = tempId.split('');
+    let x = parseInt(coordinates[0]);
+    let y = parseInt(coordinates[1]);
+    Box[x][y] = tempVal;
+    winCheck(tempVal);
+}
+
+// Check for win condition
+function winCheck(player) {
+    for (let condition of winningConditions) {
+        let winCondition = [
+            Box[Math.floor(condition[0]/3)][condition[0]%3],
+            Box[Math.floor(condition[1]/3)][condition[1]%3],
+            Box[Math.floor(condition[2]/3)][condition[2]%3]
+        ];
+        if (winCondition[0] === "" || winCondition[1] === "" || winCondition[2] === "") {
+            continue;
+        }
+        if (winCondition[0] === player && winCondition[1] === player && winCondition[2] === player) {
+            console.log("won");
+            btnRef.forEach((ele)=>{
+                ele.disabled=true;
+            })
+            return;
+        }
+    }
+    console.log("lose");
+}
